d9e669920497a8dccf946e064139086bc8e7e21f
hyemin33 <124856203+hyemin33@users.noreply.github.com>
2023-04-19 15:01:12 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let data = input.map(x => Number(x));
+let max = Math.max(...data)
+
+console.log(max)
+console.log(data.indexOf(max) + 1)
+
