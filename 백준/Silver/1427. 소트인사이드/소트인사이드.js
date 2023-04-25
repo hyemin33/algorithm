228ebaef02808556d0d023bd86d06062f1ed7da9
hyemin33 <124856203+hyemin33@users.noreply.github.com>
2023-04-25 20:19:58 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+let n = input[0];
+let cnt = Array(10).fill(0);
+
+for(let x of n){
+    cnt[Number(x)]++;
+}
+
+let answer = '';
+for(let i=9 ; i>= 0 ; i--){
+    for(let j=0;j<cnt[i]; j++) answer += i +"";
+}
+console.log(answer)
