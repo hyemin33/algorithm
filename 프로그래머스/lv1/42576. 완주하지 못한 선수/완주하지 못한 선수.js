6d729ebef1c6c1c2d62be0a3e386757284bd6d97
hyemin33 <124856203+hyemin33@users.noreply.github.com>
2025-06-17 16:35:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 55.27 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,17 @@
 function solution(participant, completion) {
     let map = new Map();
 
-    for(let i=0 ; i<participant.length; i++){
-        map.set(participant[i], (map.get(participant[i]) ?? 0) + 1);
-        map.set(completion[i], (map.get(completion[i]) ?? 0) - 1)
+    for(let i = 0; i < participant.length; i++){
+        const participantName = participant[i];
+        const completionName = completion[i];
+        map.set(participantName, (map.get(participantName) || 0) + 1);
+        
+         if(i < completion.length) {
+            map.set(completionName, (map.get(completionName) || 0) - 1)
+         }
     }
     
-    for([key,value] of map) {
+    for([key, value] of map) {
          if(value > 0) return key
     }
 }
